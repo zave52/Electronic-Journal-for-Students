d999b5881999eb9eff9e6941bbd205559c269317
PavloMolytovnyk <pavlomolytovny@icloud.com>
2025-11-03 11:34:10 +0200

    change html and ts files of login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login data:', this.loginForm.value);
+    }
+  }
 }
