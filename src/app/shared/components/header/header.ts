00047697aae7d0cadbd05b953733cc7256bd9e1c
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-11-20 21:03:40 +0200

    Refactor header component layout and implement OnPush change detection

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
 
