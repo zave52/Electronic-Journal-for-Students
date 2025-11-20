b3324dad8a5d099005c3b0c8284890897ab1febe
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-11-20 21:08:08 +0200

    Implement sidebar layout with navigation and logout button

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Sidebar {
 
