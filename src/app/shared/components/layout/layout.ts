f5237c7e8288f2dda038fc2a037f61e944bcc896
Zakhar-Savchyn <146958577+zave52@users.noreply.github.com>
2025-11-20 21:24:58 +0200

    Build the LayoutComponent UI (#6)
    * Add platform checks for localStorage access in AuthService
    
    * Refactor header component layout and implement OnPush change detection
    
    * Implement layout structure with responsive sidebar and header components
    
    * Implement sidebar layout with navigation and logout button

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Layout {
 
