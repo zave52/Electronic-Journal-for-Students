b95eb77246627a7629030c9cc660604523aef937
PavloMolytovnyk <pavlomolytovny@icloud.com>
2025-12-02 15:35:18 +0200

    Changed guard file

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,24 +1,18 @@
 import { inject } from '@angular/core';
 import {
-  CanActivateFn, // Використовуємо CanActivateFn для функціонального гуарда
+  CanActivateFn,
   Router,
 } from '@angular/router';
-import { AuthService } from '../services'; // Переконайтеся, що шлях правильний
+import { AuthService } from '../services';
 
-// Експортуємо функціональний гуард як константу
 export const authGuard: CanActivateFn = (route, state) => {
-  // Впроваджуємо AuthService та Router за допомогою функції inject()
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  // 1. Перевіряємо, чи користувач увійшов
   if (authService.isLoggedIn()) {
-    // 2. Якщо увійшов, дозволяємо активацію
     return true;
   } else {
-    // 3. Якщо не увійшов, перенаправляємо на /login
     router.navigate(['/login']);
-    // 4. Забороняємо активацію
     return false;
   }
 };
