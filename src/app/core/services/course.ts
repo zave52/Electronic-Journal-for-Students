3578c283dfa8741c1fbe86aba5c7da4cf9a98f8f
PavloMolytovnyk <pavlomolytovny@icloud.com>
2025-12-03 10:35:52 +0200

    Change course-details files(1)

--- src/app/core/services/course.ts
+++ src/app/core/services/course.ts
@@ -27,17 +27,21 @@ export class CourseService {
   getCoursesByStudentId(studentId: number): Observable<Course[]> {
     return this.http.get<Enrollment[]>(`${environment}/enrollments?studentId=${studentId}`).pipe(
       map(enrollments => enrollments.map(e => e.courseId)),
-
       switchMap(courseIds => {
         if (courseIds.length === 0) {
-          console.log(`Student ${studentId} is not enrolled in any courses.`);
           return EMPTY;
         }
-
-        const courseQueries = courseIds.map(id => `id=${id}`).join('&');
-
-        return this.http.get<Course[]>(`${environment}/courses?${courseQueries}`);
+        const q = courseIds.map(id => `id=${id}`).join('&');
+        return this.http.get<Course[]>(`${environment}/courses?${q}`);
       })
     );
+  }
+
+  getCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`${environment}/courses/${courseId}`);
+  }
+
+  getLessonsByCourseId(courseId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${environment}/lessons?courseId=${courseId}`);
   }
 }
--- src/app/features/student/assignments/assignments.ts
+++ src/app/features/student/assignments/assignments.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from '../../../../environments/environment';
+
 
 @Component({
   selector: 'app-assignments',
@@ -6,6 +11,14 @@ import { Component } from '@angular/core';
   templateUrl: './assignments.html',
   styleUrl: './assignments.css',
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class Assignments {
 
+  constructor(private http: HttpClient) {}
+
+  getAssignmentsByLessonId(lessonId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${environment}/assignments?lessonId=${lessonId}`);
+  }
 }
