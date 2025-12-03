c7a688eb06fa8d7e48a44758b52490fce4a18f77
PavloMolytovnyk <pavlomolytovny@icloud.com>
2025-12-03 11:36:58 +0200

    Change grades file(1)

--- src/app/core/services/grade.ts
+++ src/app/core/services/grade.ts
@@ -1,8 +1,18 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GradeService {
 
+  constructor(private http: HttpClient) { }
+  getGradesByStudentId(studentId: number): Observable<any[]> {
+    return this.http.get<any[]>(
+      `${environment.apiUrl}/grades?studentId=${studentId}`
+    );
+  }
 }
