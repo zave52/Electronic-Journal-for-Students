a33c4e66e4d22fb0bbbd2f6433cd17c38bfe696a
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-12-03 19:44:09 +0200

    Add UserService methods for fetching users from API

--- src/app/core/services/user.ts
+++ src/app/core/services/user.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/users`;
 
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 }
